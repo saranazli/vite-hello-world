a77a0e48bcb824444d291179c8bb7b14e9afd70c
s4r44d43 <sararez@live.it>
2024-03-20 13:57:00 +0100

    [customized] global css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+  export default {
+
+    data(){
+
+      return{ 
+
+        title: 'HELLO WORLD',
+        subtitle: 'our first lesson on node.js',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quae numquam. Quod est nam possimus ea reprehenderit laborum quam?'
+
+      }
+    }
+  }
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <h1>{{ title }}</h1>
+
+  <h2>{{ subtitle }}</h2>
+
+  <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, doloribus facere, magni perspiciatis libero voluptatum ab asperiores aliquam illum inventore saepe! Ducimus tempore doloribus maxime quo facere. Magnam, eos provident.</p>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
+  
+
 </style>
